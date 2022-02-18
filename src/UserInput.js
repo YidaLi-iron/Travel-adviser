9512e294ac4aa73aa00eeff3b9177a33bdac9790
YidaLi-iron <>
2022-02-18 11:47:50 -0800

    initial frame

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,13 +1,38 @@
 import './App.css';
 
+import './App.css';
+
 function UserInput() {
-    return(
-        <footer className="footer">
-            <form>Current location<input className="Input"></input>
-            Starting date<input className="Input"></input>
-            Destination1<input className="Input"></input>
-            Destination2<input className="Input"></input></form>
-        </footer>
+    return (
+        <div className="footer">
+            <form className="user-input-form">
+                <div className="button">
+                    <button>submit</button>
+                </div>
+                <div className="user-input-box">
+                    <div className="user-input-box1">
+                        <div>
+                            <label className="label1" htmlFor="currentLocation">Current location</label>
+                            <input id="currentLocation" className="Input"/>
+                        </div>
+                        <div>
+                            <label className="label2" htmlFor="startingDate">Starting date</label>
+                            <input id="startingDate" className="Input"/>
+                        </div>
+                    </div>
+                    <div className="user-input-box2">
+                        <div>
+                            <label className="label1" htmlFor="dest1">Destination1</label>
+                            <input id="dest1" className="Input"/>
+                        </div>
+                        <div>
+                            <label className="label2" htmlFor="dest2">Destination2</label>
+                            <input id="dest2" className="Input"/>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
     );
 }
 
