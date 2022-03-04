dad3a58622d64528055ad0a09f94dfffc5fc573a
YidaLi-iron <>
2022-03-04 15:01:37 -0800

    week9 update

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,7 +1,8 @@
 import './App.css';
-import React, { Component } from 'react';
-import { useState } from 'react';
+import React, {Component} from 'react';
+import {useState} from 'react';
 import Analyze from "./Analyze";
+import {dialogActionsClasses} from "@mui/material";
 
 function UserInput() {
     // state = {
@@ -11,106 +12,104 @@ function UserInput() {
     //     dest2: '',
     //     data: []
     // };
-    const[currentLocation, setCurrentLocation] = useState("");
-    const[startingDate, setstartingDate] = useState("");
-    const[dest1, setDest1] = useState("");
-    const[dest2, setDest2] = useState("");
-    // const[data, setData] = useState({});
-    // const[data1, setData1] = useState({});
-    // const[data2, setData2] = useState({});
-    const[isSubmit, setisSubmit] = useState(false);
+    const [currentLocation, setCurrentLocation] = useState("");
+    const [startingDate, setstartingDate] = useState("");
+    const [dest1, setDest1] = useState("");
+    const [dest2, setDest2] = useState("");
+    const [data, setData] = useState({});
+    const [data1, setData1] = useState({});
+    const [data2, setData2] = useState({});
+    const [isSubmit, setisSubmit] = useState(false);
+
     // data = [];
 
     function handleFormSubmit() {
-        // let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+dest1+"?unitGroup=metric&key=ZPCPEUHNFPNKCPGAW85X3MPFS&contentType=json"
-        // fetch(url, {
-        //     "method": "GET",
-        //     "headers": {
-        //     }
-        // })
-        //     .then(response => { 
-        //         return response.json()
-        //     })
-        //     .then(function(res){
-        //         setData(res)
-        //     })
-        //     .catch(err => {
-        //         console.error(err);
-        //     });
-        //     console.log(data)
+        let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + dest1 + "?unitGroup=metric&key=E2BMHT6HB87MD893JXHUKR2N4&contentType=json"
+        fetch(url, {
+            "method": "GET",
+            "headers": {}
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(function (res) {
+                setData(res)
+            })
+            .catch(err => {
+                console.error(err);
+            });
 
-        //     let url1 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+dest2+"?unitGroup=metric&key=ZPCPEUHNFPNKCPGAW85X3MPFS&contentType=json"
-        //     fetch(url1, {
-        //         "method": "GET",
-        //         "headers": {
-        //         }
-        //     })
-        //         .then(response => { 
-        //             return response.json()
-        //         })
-        //         .then(function(res){
-        //             setData1(res)
-        //         })
-        //         .catch(err => {
-        //             console.error(err);
-        //         });
-        //         console.log(data1)
+        let url1 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + dest2 + "?unitGroup=metric&key=E2BMHT6HB87MD893JXHUKR2N4&contentType=json"
+        fetch(url1, {
+            "method": "GET",
+            "headers": {}
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(function (res) {
+                setData1(res)
+            })
+            .catch(err => {
+                console.error(err);
+            });
 
-        //         let url2 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+currentLocation+"?unitGroup=metric&key=ZPCPEUHNFPNKCPGAW85X3MPFS&contentType=json"
-        //         fetch(url2, {
-        //             "method": "GET",
-        //             "headers": {
-        //             }
-        //         })
-        //             .then(response => { 
-        //                 return response.json()
-        //             })
-        //             .then(function(res){
-        //                 console.log(res)
-        //                 setData2(res)
-        //                 console.log(data2)
-        //             })
-        //             .catch(err => {
-        //                 console.error(err);
-        //             });
-        //             console.log(data2)
+        let url2 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + currentLocation + "?unitGroup=metric&key=E2BMHT6HB87MD893JXHUKR2N4&contentType=json"
+        fetch(url2, {
+            "method": "GET",
+            "headers": {}
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(function (res) {
+                setData2(res)
+            })
+            .catch(err => {
+                console.error(err);
+            });
 
-                    setisSubmit(true)
-        
-    };  
+        setTimeout(()=>setisSubmit(true),10000)
 
-        return (
-            <div className="footer">
-            {isSubmit && <Analyze currentLocation={currentLocation} dest1={dest1} dest2={dest2}>a a a</Analyze>}
-                <form className="user-input-form" action='#'>
-                    <div className="button">
-                        <button onClick={handleFormSubmit}>submit</button>
-                    </div>
-                    <div className="user-input-box">
-                        <div className="user-input-box1">
-                            <div>
-                                <label className="label1" htmlFor="currentLocation">Current location</label>
-                                <input id="currentLocation" className="Input" value={currentLocation} onChange={(e) => setCurrentLocation(e.target.value)} />
-                            </div>
-                            <div>
-                                <label className="label2" htmlFor="startingDate">Starting date</label>
-                                <input id="startingDate" className="Input" value={startingDate} onChange={(e) => setstartingDate(e.target.value)} />
-                            </div>
+    };
+
+    return (
+        <div className="footer">
+            {isSubmit && <Analyze data={data} data1={data1} data2={data2} startingDate={startingDate} dest={dest1}/>}
+            <div>{data.latitude}</div>
+            <form className="user-input-form" action='#'>
+                <div className="button">
+                    <button onClick={handleFormSubmit}>submit</button>
+                </div>
+                <div className="user-input-box">
+                    <div className="user-input-box1">
+                        <div>
+                            <label className="label1" htmlFor="currentLocation">Current location</label>
+                            <input id="currentLocation" className="Input" value={currentLocation}
+                                   onChange={(e) => setCurrentLocation(e.target.value)}/>
                         </div>
-                        <div className="user-input-box2">
-                            <div>
-                                <label className="label1" htmlFor="dest1">Destination1</label>
-                                <input id="dest1" className="Input" value={dest1} onChange={(e) => setDest1(e.target.value)} />
-                            </div>
-                            <div>
-                                <label className="label2" htmlFor="dest2">Destination2</label>
-                                <input id="dest2" className="Input" value={dest2} onChange={(e) => setDest2(e.target.value)} />
-                            </div>
+                        <div>
+                            <label className="label2" htmlFor="startingDate">Starting date</label>
+                            <input id="startingDate" className="Input" value={startingDate}
+                                   onChange={(e) => setstartingDate(e.target.value)}/>
                         </div>
                     </div>
-                </form>
-            </div>
-        );
-    }
+                    <div className="user-input-box2">
+                        <div>
+                            <label className="label1" htmlFor="dest1">Destination1</label>
+                            <input id="dest1" className="Input" value={dest1}
+                                   onChange={(e) => setDest1(e.target.value)}/>
+                        </div>
+                        <div>
+                            <label className="label2" htmlFor="dest2">Destination2</label>
+                            <input id="dest2" className="Input" value={dest2}
+                                   onChange={(e) => setDest2(e.target.value)}/>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
 
 export default UserInput;
--- src/analyze.js
+++ src/analyze.js
@@ -2,185 +2,94 @@ import {useState} from "react";
 import axios from "axios";
 
 function Analyze(props) {
-    const[data, setData] = useState({});
-    const[data1, setData1] = useState({});
-    const[data2, setData2] = useState({});
-    const [precip, setPrecip] = useState([]);
-    const [humidity, setHumidity] = useState([]);
-    const [windspeed, setWindspeed] = useState([]);
+    const [data, setData] = useState(props.data);
+    const [data1, setData1] = useState(props.data1);
+    const [data2, setData2] = useState(props.data2);
+    const [startingDate, setstartingDate] = useState(props.startingDate);
+    // const [precip, setPrecip] = useState([]);
+    // const [humidity, setHumidity] = useState([]);
+    // const [windspeed, setWindspeed] = useState([]);
+    console.log(data2)
+    // console.log(data2)
 
-    let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+props.dest1+"?unitGroup=metric&key=ZPCPEUHNFPNKCPGAW85X3MPFS&contentType=json"
-    fetch(url, {
-        "method": "GET",
-        "headers": {
+    let precipprob, humidity, windspeed
+    for (let day of data.days) {
+        if (day.datetime == startingDate) {
+            precipprob = day.precipprob
+            humidity = day.humidity
+            windspeed = day.windspeed
+            break;
         }
-    })
-        .then(response => { 
-            return response.json()
-        })
-        .then(function(res){
-    
-            setData(res)
-        })
-        .catch(err => {
-            console.error(err);
-        });
-        console.log(data)
-
-        let url1 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+props.dest2+"?unitGroup=metric&key=ZPCPEUHNFPNKCPGAW85X3MPFS&contentType=json"
-        fetch(url1, {
-            "method": "GET",
-            "headers": {
-            }
-        })
-            .then(response => { 
-                return response.json()
-            })
-            .then(function(res1){
-                setData1(res1)
-            })
-            .catch(err => {
-                console.error(err);
-            });
-            console.log(data1)
-
-            let url2 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+props.currentLocation+"?unitGroup=metric&key=ZPCPEUHNFPNKCPGAW85X3MPFS&contentType=json"
-            fetch(url2, {
-                "method": "GET",
-                "headers": {
-                }
-            })
-                .then(response => { 
-                    return response.json()
-                })
-                .then(function(res2){
-                    console.log(res2)
-                    setData2(res2)
-                    console.log(data2)
-                })
-                .catch(err => {
-                    console.error(err);
-                });
-                console.log(data2)
-
-
-
-
-
-    for(let value of Object.values(data['days'])) {
-        for (let precipprob of Object.values(value['precipprob'])) {
-            setPrecip([...precip, precipprob])
-        }
-
-        for (let humid of Object.values(value['humidity'])) {
-            setHumidity([...humidity, humid])
-        }
-
-        for (let wind of Object.values(value['windspeed'])) {
-            setWindspeed([...windspeed, wind])
-        }
-
     }
 
-    for(let value of Object.values(data1['days'])) {
-        for (let precipprob of Object.values(value['precipprob'])) {
-            setPrecip([...precip, precipprob])
+    let precipprob1, humidity1, windspeed1
+    for (let day of data1.days) {
+        if (day.datetime == startingDate) {
+            precipprob1 = day.precipprob
+            humidity1 = day.humidity
+            windspeed1 = day.windspeed
+            break;
         }
-
-        for (let humid of Object.values(value['humidity'])) {
-            setHumidity([...humidity, humid])
-        }
-
-        for (let wind of Object.values(value['windspeed'])) {
-            setWindspeed([...windspeed, wind])
-        }
-
     }
 
+    //
+    // for (let value of Object.values(data1['days'])) {
+    //     for (let precipprob of Object.values(value['precipprob'])) {
+    //         setPrecip([...precip, precipprob])
+    //     }
+    //
+    //     for (let humid of Object.values(value['humidity'])) {
+    //         setHumidity([...humidity, humid])
+    //     }
+    //
+    //     for (let wind of Object.values(value['windspeed'])) {
+    //         setWindspeed([...windspeed, wind])
+    //     }
+    //
+    // }
+    //
     const distance1 = {
         method: 'GET',
         url: 'https://trueway-matrix.p.rapidapi.com/CalculateDrivingMatrix',
         params: {
-          origins: Object.values(data2['latitude']) + ',' + Object.values(data2['longitude']),
-          destinations: Object.values(data['latitude']) + ',' + Object.values(data['longitude'])
+            origins: data2.latitude + ',' + data2.longitude,
+            destinations: data.latitude + ',' + data.longitude
         },
         headers: {
-          'x-rapidapi-host': 'trueway-matrix.p.rapidapi.com',
-          'x-rapidapi-key': '5af58ad3e2msh6fa59a7265f9571p101d5djsn359c9e7638ec'
+            'x-rapidapi-host': 'trueway-matrix.p.rapidapi.com',
+            'x-rapidapi-key': '5af58ad3e2msh6fa59a7265f9571p101d5djsn359c9e7638ec'
         }
-      };
-      
-      axios.request(distance1).then(function (response) {
-          console.log(response.data);
-      }).catch(function (error) {
-          console.error(error);
-      });
+    };
 
-      const distance2 = {
+    axios.request(distance1).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+    
+    const distance2 = {
         method: 'GET',
         url: 'https://trueway-matrix.p.rapidapi.com/CalculateDrivingMatrix',
         params: {
-          origins: Object.values(data2['latitude']) + ',' + Object.values(data2['longitude']),
-          destinations: Object.values(data1['latitude']) + ',' + Object.values(data1['longitude'])
+            origins: data2.latitude + ',' + data2.longitude,
+            destinations: data1.latitude + ',' + data1.longitude
         },
         headers: {
-          'x-rapidapi-host': 'trueway-matrix.p.rapidapi.com',
-          'x-rapidapi-key': '5af58ad3e2msh6fa59a7265f9571p101d5djsn359c9e7638ec'
+            'x-rapidapi-host': 'trueway-matrix.p.rapidapi.com',
+            'x-rapidapi-key': '5af58ad3e2msh6fa59a7265f9571p101d5djsn359c9e7638ec'
         }
-      };
-      
-      axios.request(distance2).then(function (response) {
-          console.log(response.data);
-      }).catch(function (error) {
-          console.error(error);
-      });
+    };
+    
+    axios.request(distance2).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
 
-      for (let value of precip.slice(0,15)) {
-          var temp1 = temp1 + value
-      }
-      var precipprobability = temp1/15;
-      precipprobability = precipprobability.toFixed(2);
-
-      for (let value of precip.slice(15,30)) {
-        var temp2 = temp2 + value
-    }
-    var precipprobability2 = temp2/15;
-    precipprobability2 = precipprobability2.toFixed(2);
-
-    for (let value of humidity.slice(0,15)) {
-        var temp3 = temp3 + value
-    }
-    var avehumidity1 = temp3/15;
-    avehumidity1 = avehumidity1.toFixed(2);
-
-    for (let value of humidity.slice(15,30)) {
-        var temp4 = temp4 + value
-    }
-    var avehumidity2 = temp4/15;
-    avehumidity2 = avehumidity2.toFixed(2);
-
-    for (let value of windspeed.slice(0,15)) {
-        var temp5 = temp5 + value
-    }
-    var avewindspeed1 = temp5/15;
-    avewindspeed1 = avewindspeed1.toFixed(2);
-
-    for (let value of windspeed.slice(15,30)) {
-        var temp6 = temp6 + value
-    }
-    var avewindspeed2 = temp6/15;
-    avewindspeed2 = avewindspeed2.toFixed(2);
-
-    return(
-        <div></div>
+    return (
+        <div>bbb</div>
     );
 
 }
-
-// Analyze.defaultProps = {
-//     data: "1",
-//     data1: "2",
-//     data2: "3"
-//   }
 
 export default Analyze;
